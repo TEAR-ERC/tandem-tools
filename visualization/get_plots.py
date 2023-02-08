05604116254bd93c1a09e4969b37145a8c2fefb4
JeenaYun <j4yun@ucsd.edu>
2023-02-07 16:06:16 -0800

    Enable intermediate slip rate contour

--- visualization/get_plots.py
+++ visualization/get_plots.py
@@ -15,6 +15,7 @@ myblue = (118/255,177/255,230/255)
 myburgundy = (214/255,0,0)
 mynavy = (17/255,34/255,133/255)
 yr2sec = 365*24*60*60
+wk2sec = 7*24*60*60
 
 # Set input parameters -------------------------------------------------------------------------------------------------------------------
 # ***
@@ -23,21 +24,25 @@ yr2sec = 365*24*60*60
 # save_figs: select which figure you want to save
 #            For the first three plots, non-zero values would indicate its desired depth (surface should be 0.01)
 # plot_in_sec: choose whether you want the time axis to be in year or in seconds
-# Vths: a threshold value for the slip rate to be defined as coseismic [m/s]
-# tcreep: Contour line interval for the creeping section. Only required when cumslip plotting is on
-# tcoseismic: Contour line interval for the coseismic section. Only required when cumslip plotting is on
+# Vths, Vlb: lower- and upper bound for the slip rate. [m/s] Used when plotting the cumslip plot. 
+#            Vub will be used as coseismic slip rate threshold. Give Vlb = 0 when intermediate section is unnecessary.
+# dt_creep: Contour line interval for the creeping section. Only required when cumslip plotting is on [s]
+# dt_coseismic: Contour line interval for the coseismic section. Only required when cumslip plotting is on [s]
 # mingap: minimum seperation time between two different events. Used only for plotting purpose.
+# dt_interm (optional): Contour line interval for the intermediate section. Only required when cumslip plotting is on [s]. Give zero if unnecessary.
 # cuttime (optional): show result up until to the given time to save computation time. Give zero if unnecessary.
 # Wf (optional): depth of fault. Used only for plotting purpose (to set ylim). Give zero if unnecessary.
 # ***
-save_dir = '/export/dump/jyun/DZ_triangular_homogeneous'
+save_dir = '/export/dump/jyun/DZ_triangular'
 compute = 0
 save_figs = [0.01, 2, 0, 0, 1] # Slip rate vs. Time | Slip vs. Time | Stress vs. Time | Initial stress | Cumulative slip vs. Depth
 plot_in_sec = 0
 Vths = 1e-2
-tcreep = 2*yr2sec
-tcoseismic = 1
+Vlb = 1e-8
+dt_creep = 2*yr2sec
+dt_coseismic = 1
 mingap = 60
+dt_interm = 1*wk2sec
 cuttime = 700*yr2sec
 Wf = 24
 
@@ -45,7 +50,7 @@ Wf = 24
 if compute:
     print('Compute on - extract outputs...',end=' ')
 
-    fnames = glob.glob('%s/%s/outputs/*dp*.csv'%(save_dir))
+    fnames = glob.glob('%s/outputs/*dp*.csv'%(save_dir))
     if len(fnames) == 0:
         raise NameError('No such file found - check the input')
     
@@ -71,14 +76,14 @@ if compute:
     print('done!')
 
     print('Save data...',end=' ')
-    np.save('%s/%s/outputs'%(save_dir),outputs)
-    np.save('%s/%s/outputs_depthinfo'%(save_dir),dep)
+    np.save('%s/outputs'%(save_dir),outputs)
+    np.save('%s/outputs_depthinfo'%(save_dir),dep)
     print('done!')
 else:
-    print('Load saved data: %s/%s/outputs'%(save_dir))
-    print('Load saved data: %s/%s/outputs_depthinfo'%(save_dir))
-    outputs = np.load('%s/%s/outputs.npy'%(save_dir))
-    dep = np.load('%s/%s/outputs_depthinfo.npy'%(save_dir))
+    print('Load saved data: %s/outputs'%(save_dir))
+    print('Load saved data: %s/outputs_depthinfo'%(save_dir))
+    outputs = np.load('%s/outputs.npy'%(save_dir))
+    dep = np.load('%s/outputs_depthinfo.npy'%(save_dir))
 
 # Slip rate vs. Time ---------------------------------------------------------------------------------------------------------------------
 if abs(save_figs[0])>0:
@@ -108,7 +113,7 @@ if abs(save_figs[0])>0:
     else:
         plt.title('Depth = %1.1f [km]'%target_depth,fontsize=20,fontweight = 'bold')
     plt.tight_layout()
-    plt.savefig('%s/%s/sliprate.png'%(save_dir),dpi=300)
+    plt.savefig('%s/sliprate.png'%(save_dir),dpi=300)
 
 # Slip vs. Time --------------------------------------------------------------------------------------------------------------------------
 if abs(save_figs[1])>0:
@@ -132,7 +137,7 @@ if abs(save_figs[1])>0:
     else:
         plt.title('Depth = %1.1f [km]'%target_depth,fontsize=20,fontweight = 'bold')
     plt.tight_layout()
-    plt.savefig('%s/%s/slip.png'%(save_dir))
+    plt.savefig('%s/slip.png'%(save_dir))
 
 # Stress vs. Time ------------------------------------------------------------------------------------------------------------------------
 if abs(save_figs[2])>0:
@@ -161,7 +166,7 @@ if abs(save_figs[2])>0:
         ax[0].set_title('Depth = %1.1f [km]'%target_depth,fontsize=20,fontweight = 'bold')
         ax[1].set_title('Depth = %1.1f [km]'%target_depth,fontsize=20,fontweight = 'bold')
     plt.tight_layout()
-    plt.savefig('%s/%s/stresses.png'%(save_dir))
+    plt.savefig('%s/stresses.png'%(save_dir))
 
 # Initial stress vs. Time ------------------------------------------------------------
 if save_figs[3]:
@@ -178,18 +183,18 @@ if save_figs[3]:
         c += 1
 
     plt.rcParams['font.size'] = '15'
-    fig,ax = plt.subplots(figsize=(9,7))
-    ax.scatter(sigma,z,lw=2.5,color=mynavy,label='Normal Stress',zorder=3)
-    ax.scatter(tau,z,lw=2.5,color=myburgundy,label='Shear Stress',zorder=3)
-    ax.set_xlabel('Stress [MPa]',fontsize=17)
-    ax.set_ylabel('Depth [km]',fontsize=17)
+    plt.figure(figsize=(9,7))
+    plt.scatter(sigma,z,lw=2.5,color=mynavy,label='Normal Stress',zorder=3)
+    plt.scatter(tau,z,lw=2.5,color=myburgundy,label='Shear Stress',zorder=3)
+    plt.xlabel('Stress [MPa]',fontsize=17)
+    plt.ylabel('Depth [km]',fontsize=17)
     if abs(Wf) > 1e-3:
-        ax.set_ylim(0,Wf)
-    ax.invert_yaxis()
+        plt.ylim(0,Wf)
+    plt.gca().invert_yaxis()
     plt.grid(True)
-    ax.legend(fontsize=13,loc='lower left')
+    plt.legend(fontsize=13,loc='lower left')
     plt.tight_layout()
-    plt.savefig('%s/%s/stressvsdepth.png'%(save_dir))
+    plt.savefig('%s/stressvsdepth.png'%(save_dir))
 
 # Cumslip vs. Depth ----------------------------------------------------------------------------------------------------------------------
 if save_figs[4]:
@@ -198,8 +203,16 @@ if save_figs[4]:
     depcreep = []
     cscoseis = []
     depcoseis = []
+    if dt_interm > 0:        
+        csinterm = []
+        depinterm = []
 
-    def event_times(dep,outputs,Vths,cuttime):
+    if abs(cuttime) < 1e-3:
+        print('No cutting')
+    else:
+        print('Cut at %2.1f yr'%(cuttime/yr2sec))
+
+    def event_times(dep,outputs,Vlb,Vths,cuttime):
         '''
         Search for the earliest start of the event and the last stop of the event.
         (EXPLANATION - you may read it once and remove it as you wish)
@@ -232,8 +245,15 @@ if save_figs[4]:
                     cumslip = cumslip[time <= cuttime]
                     time = time[time <= cuttime]
 
-            # Define events by sliprate rager than certain threshold
-            events=np.where(sliprate > Vths)[0]
+            # Define events by sliprate
+            if Vlb > 0:
+                if c == 0:
+                    print('%1.0e < Slip rate < %1.0e'%(Vlb,Vths))
+                events = np.where(np.logical_and(sliprate < Vths,sliprate > Vlb))[0]
+            else:
+                if c == 0:
+                    print('Slip rate > %1.0e'%(Vths))
+                events = np.where(sliprate > Vths)[0]
 
             if len(events) == 0:
                 print('Depth',z,' - no events')
@@ -349,36 +369,38 @@ if save_figs[4]:
                     tstart = np.copy(new_tstart[ii])
                     tend = np.copy(new_tend[ii])
                     evdep = np.copy(new_evdep[ii])
-
             c += 1
+
+        # If there are too close events, merge them as one
+        new_tstart = []
+        new_tend = []
+        new_evdep = []
+        u = 0
+        while u < len(tstart):
+            nearest = np.where(tstart[tstart > tstart[u]] - tstart[u] <= mingap)[0] + u + 1
+            if len(nearest) != 0:
+                new_tstart.append(tstart[u])
+                new_tend.append(tend[max(nearest)])
+                new_evdep.append(evdep[u])
+                u += len(nearest)+1
+            else:
+                new_tstart.append(tstart[u])
+                new_tend.append(tend[u])
+                new_evdep.append(evdep[u])
+                u += 1
+
+        tstart = np.copy(new_tstart)
+        tend = np.copy(new_tend)
+        evdep = np.copy(new_evdep)
+
         return tstart, tend, evdep
 
     # Obtain globally min. event start times and max. event tend times
-    tstart, tend, evdep = event_times(dep,outputs,Vths,cuttime)
+    if dt_interm > 0:
+        tstart_interm, tend_interm, evdep = event_times(dep,outputs,Vlb,Vths,cuttime)
+    tstart_coseis, tend_coseis, evdep = event_times(dep,outputs,0,Vths,cuttime)
+    evslip = np.zeros(tstart_coseis.shape)
 
-    # If there are too close events, merge them as one
-    new_tstart = []
-    new_tend = []
-    new_evdep = []
-    u = 0
-    while u < len(tstart):
-        nearest = np.where(tstart[tstart > tstart[u]] - tstart[u] <= mingap)[0] + u + 1
-        if len(nearest) != 0:
-            new_tstart.append(tstart[u])
-            new_tend.append(tend[max(nearest)])
-            new_evdep.append(evdep[u])
-            u += len(nearest)+1
-        else:
-            new_tstart.append(tstart[u])
-            new_tend.append(tend[u])
-            new_evdep.append(evdep[u])
-            u += 1
-
-    tstart = np.copy(new_tstart)
-    tend = np.copy(new_tend)
-    evdep = np.copy(new_evdep)
-
-    evslip = np.zeros(tstart.shape)
     # Now interpolate the cumulative slip using given event time ranges
     for i in np.argsort(abs(dep)):
         z = abs(dep[i])
@@ -394,15 +416,27 @@ if save_figs[4]:
         f = interpolate.interp1d(time,cumslip)
 
         # -------------------- Creep
-        t5yr = np.arange(time[0],time[-1],tcreep)
-        cscreep.append(f(t5yr))
-        depcreep.append(z*np.ones(len(t5yr)))
+        tcreep = np.arange(time[0],time[-1],dt_creep)
+        cscreep.append(f(tcreep))
+        depcreep.append(z*np.ones(len(tcreep)))
         
+        # -------------------- Inter
+        if dt_interm > 0:
+            cs = []
+            depth = []
+            for j in range(len(tstart_interm)):
+                tinterm = np.arange(tstart_interm[j],tend_interm[j],dt_interm)
+                cs.append(f(tinterm))
+                depth.append(z*np.ones(len(tinterm)))
+
+            csinterm.append([item for sublist in cs for item in sublist])
+            depinterm.append([item for sublist in depth for item in sublist])
+
         # -------------------- Coseismic
         cs = []
         depth = []
-        for j in range(len(tstart)):
-            tcoseis = np.arange(tstart[j],tend[j],tcoseismic)
+        for j in range(len(tstart_coseis)):
+            tcoseis = np.arange(tstart_coseis[j],tend_coseis[j],dt_coseismic)
             cs.append(f(tcoseis))
             depth.append(z*np.ones(len(tcoseis)))
 
@@ -412,13 +446,15 @@ if save_figs[4]:
         # -------------------- Event detph
         if np.isin(z,evdep):
             indx = np.where(z==evdep)[0]
-            evslip[indx] = f(tstart)[indx]
+            evslip[indx] = f(tstart_coseis)[indx]
 
     # --- Plot the result
     plt.rcParams['font.size'] = '27'
     plt.figure(figsize=(18,11))
 
     plt.plot(cscreep,depcreep,color='royalblue',lw=1)
+    if dt_interm > 0:
+        plt.plot(csinterm,depinterm,color='yellowgreen',lw=1)
     plt.plot(cscoseis,depcoseis,color='chocolate',lw=1)
     ev = plt.scatter(evslip,evdep,marker='*',s=700,facecolor=myburgundy,edgecolors='k',lw=2,zorder=3)
     plt.legend([ev],['Hypocenters'],fontsize=25,framealpha=1)
@@ -427,9 +463,9 @@ if save_figs[4]:
     xl = plt.gca().get_xlim()
     plt.xlim(0,xl[1])
     if abs(Wf) > 1e-3:
-        ax.set_ylim(0,Wf)
+        plt.ylim(0,Wf)
     plt.gca().invert_yaxis()
 
     plt.tight_layout()
-    plt.savefig('%s/%s/cumslip_%d_%d.png'%(save_dir,int(Vths*100),int(tcoseismic*10)),dpi=300)
+    plt.savefig('%s/cumslip_%d_%d.png'%(save_dir,int(Vths*100),int(dt_coseismic*10)),dpi=300)
 
