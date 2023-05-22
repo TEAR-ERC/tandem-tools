91765a794a049b2a7a14b56bfbc184528881b098
Jeena Yun <j4yun@ucsd.edu>
2023-05-22 16:49:14 -0700

    updated plotting script

--- visualization/create_plot.sh
+++ visualization/create_plot.sh
@@ -1,9 +1,9 @@
 # Example code to obtain the plot
 # e.g., save_dir=/your/path/to/simulation/result
-save_dir='.'
+save_dir='examples'
 
 # Example 1. Extract and save essential part from outputs, and returns cumulative slip vs. depth plot, event analyzing plot and image of slip rate in timesteps
-python get_plots.py $save_dir -c -wf 24 -csl -dtcr 2 -dtco 0.5 -imsr -ts -evan
+python get_plots.py $save_dir -c -wf 40 -csl -dtcr 2 -dtco 0.5 -imsr -ts -evan
 
 # Example 2. Returns cumulative slip vs. depth plot with spin-up after 2.5 m of slip and depth histogram on the side
 # python get_plots.py $save_dir -csl -dtcr 2 -dtco 0.5 -spup 2.5 -dd
